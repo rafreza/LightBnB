0eb9cadd857ca1f7cb2a2d039332bab3f605ae6e
Rafin Reza <rafintokyo@gmail.com>
2021-02-05 06:05:56 +0000

    fetch user data for login and registration

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -15,18 +15,14 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+const getUserWithEmail = function (email) {
+  return pool.query(`SELECT * FROM users WHERE email = $1`, [email]).then(user => {
+    return user.rows[0];
+  }).catch(() => {
+    return null;
+  });
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -34,9 +30,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+const getUserWithId = function (id) {
+  return pool.query(`SELECT * FROM users WHERE id = $1`, [id]).then(user => {
+    return user.rows[0];
+  }).catch(() => {
+    return null;
+  });
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -45,12 +45,14 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+const addUser = function (user) {
+  return pool.query(`INSERT INTO users(name, password, email) VALUES($1, $2, $3) RETURNING *`,
+  [user.name, user.password, user.email]).then(user => {
+    return user.rows[0];
+  }).catch(() => {
+    return null;
+  });
+};
 exports.addUser = addUser;
 
 /// Reservations
